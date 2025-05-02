7aee8e279a512eb182809bbc25e6c51126fa34b0
Yao041021 <abc372311@gmail.com>
2025-05-02 10:25:05 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,10 +10,15 @@ function setup() {
 
 function draw() {
   background('#9E2A2B'); // 確保背景顏色持續更新
-  image(capture, (width - capture.width) / 2, (height - capture.height) / 2); // 將影像顯示在視窗中間
+  push(); // 儲存當前繪圖設定
+  translate(width, 0); // 將原點移動到畫布右上角
+  scale(-1, 1); // 水平翻轉畫布
+  image(capture, (width - capture.width) / 2, (height - capture.height) / 2); // 繪製翻轉後的影像
+  pop(); // 恢復繪圖設定
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight); // 當視窗大小改變時，調整畫布大小
   capture.size(windowWidth * 0.8, windowHeight * 0.8); // 重新調整影像大小
 }
+
